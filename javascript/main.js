dbeea26880f88b08cc981923b40eb8deaddac869
Hamed ATMANE <Ahmedgithub@gmx.fr>
2024-05-03 11:16:48 +0200

    Création du fichier main.js, liaison dans le fichier index.html - implémentation du main.js pour la gestion des rubriques dans la section 'About Me' (fonction opentab())

---
+++ javascript/main.js
@@ -0,0 +1,15 @@
+const tablinks = document.querySelectorAll(".tab-links");
+const tabcontents = document.querySelectorAll(".tab-contents");
+
+function opentab (tabname) {
+    for (tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+   
+    for (tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    console.log(tabcontent)
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
